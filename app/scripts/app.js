9c9a66ea1442e5221911bbe3e70406eb268a22ff
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-09 11:22:00 -0700

    feat: Implemented fetch

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,6 +21,7 @@ Instructions:
   function addSearchHeader(response) {
     try {
       response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
+      //response = getJSON(response);
     } catch (e) {
       // it's 'unknown', so leave it alone
     }
@@ -33,29 +34,12 @@ Instructions:
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    /*
-    This code needs to get wrapped in a Promise!
-     */
-    return new Promise(function(resolve, reject) {
-      var req = new XMLHttpRequest();
-      req.open('GET', url);
-      req.onload = function() {
-        if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-          resolve(req.response);
-        } else {
-        // It failed :(
-        // Be nice and reject with req.statusText
-          reject(Error(req.statusText));
-        };
-      };
-      req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-        reject(Error('Network Error'));
-      };
-      req.send();
+    return fetch(url);
+  };
+
+  function getJSON(url) {
+    return get(url).then(function(response) {
+      return response.json();
     });
   };
 
@@ -66,9 +50,10 @@ Instructions:
     You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
     pass 'unknown' to addSearchHeader if it rejects.
      */
-    get('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
     .then(function(response) {
-      addSearchHeader(response);
+      addSearchHeader(response.query);
+      console.log(response);
     })
     .catch(function(error) {
       addSearchHeader('unknown');
